2fac6a83da200e830bda2a6e38db1f10d854e616
Bjørn Erik Pedersen <bjorn.erik.pedersen@gmail.com>
2021-08-11 11:50:20 +0200

    Rewrite, remove unused

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 rm -rf public
 hugo
-s3deploy -source=public/ -region=eu-west-1 -bucket=bep.is
+s3deploy -source=public/ -region=eu-west-1 -bucket=bep.is -public-access -distribution-id=E1TX7TXFKWFR6S
